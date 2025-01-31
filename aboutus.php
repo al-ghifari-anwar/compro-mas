5e5f7eabed23dd41b4f1b72f4da3d580b4128a7b
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-01-31 18:54:17 +0700

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nova</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">nova</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-right" id="navbarNav">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="portofolio.php">Portofolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="aboutus.php">About Us</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="hero">
+        <img src="image/banner.jpg" alt="Banner">
+        <div class="hero-content">
+            <h3>Nova: Crafting Innovation, One Code at a Time</h3>
+            <p>Empowering businesses with tailored software solutions that drive success</p>
+            <a href="#service">
+                <button type="button" class="btn btn-dark btn-lg">See Service</button>
+            </a>
+        </div>
+    </div>
+
+    <div class="container pt-5 pb-5">
+        <div class="row align-items-start">
+            <!-- Kolom Teks -->
+            <div class="col-md-6">
+                <h2 class="display-3" id="aboutus">About Us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt tenetur earum magnam autem doloremque quo vel maxime numquam repudiandae fugit aspernatur, alias in dolorem inventore eos esse mollitia deleniti voluptas.
+                </p>
+                <a href="#contact">
+                <button type="button" class="btn btn-dark btn-lg mt-5">Contact Us</button>
+                </a>
+            </div>
+            <!-- Kolom Gambar -->
+            <div class="col-md-6 text-center">
+                <img src="image/aboutus.jpg" class="img-fluid crop-img" alt="About Us">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<footer class="custom-footer">
+        <div class="container">
+          <div class="row">
+            <!-- Section 1 -->
+            <div class="col-md-4 mb-4">
+              <h5>About Us</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Curabitur ac nisi id lacus blandit scelerisque. Fusce volutpat 
+                dignissim felis, nec iaculis libero dapibus sit amet.
+              </p>
+              <p>
+                Quisque vitae ligula id turpis pellentesque faucibus. 
+                Nullam sit amet velit vel sapien tincidunt tristique.
+              </p>
+            </div>
+    
+            <!-- Section 2 -->
+            <div class="col-md-4 mb-4">
+              <h5>Quick Links</h5>
+              <ul class="list-unstyled">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+              </ul>
+            </div>
+    
+            <!-- Section 3 -->
+            <div class="col-md-4 mb-4">
+              <h5>Contact Us</h5>
+              <p>
+                Address: 1234 Street Name, City, Country <br>
+                Phone: <a href="tel:+123456789">+123 456 789</a> <br>
+                Email: <a href="mailto:info@yourcompany.com">info@yourcompany.com</a>
+              </p>
+              <p>Follow us on:</p>
+              <a href="https://facebook.com" target="_blank" class="me-2">Facebook</a>
+              <a href="https://twitter.com" target="_blank" class="me-2">Twitter</a>
+              <a href="https://instagram.com" target="_blank">Instagram</a>
+            </div>
+          </div>
+    
+          <hr class="bg-white">
+          <div class="text-center">
+            <p class="mb-0">&copy; 2025 Your Company Name. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+
+      <a href="https://wa.me/123456789" target="_blank" class="whatsapp-float">
+    <img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt="WhatsApp" />
+</a>
+ 
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nova</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">nova</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-right" id="navbarNav">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="portofolio.php">Portofolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="aboutus.php">About Us</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="hero">
+        <img src="image/banner.jpg" alt="Banner">
+        <div class="hero-content">
+            <h3>Nova: Crafting Innovation, One Code at a Time</h3>
+            <p>Empowering businesses with tailored software solutions that drive success</p>
+            <a href="#service">
+                <button type="button" class="btn btn-dark btn-lg">See Service</button>
+            </a>
+        </div>
+    </div>
+
+    <div class="container-fluid Service pt-5 pb-5">
+        <div class="container text-center">
+            <h2 class="display-3" id="service">Service</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nesciunt.</p>
+
+            <div class="row">
+                <!-- Kolom pertama -->
+                <div class="col-md-4">
+                    <span class="circle">
+                        <svg width="100px" height="100px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8.01005 0.858582L6.01005 14.8586L7.98995 15.1414L9.98995 1.14142L8.01005 0.858582Z" fill="#ffffff"></path> <path d="M12.5 11.5L11.0858 10.0858L13.1716 8L11.0858 5.91422L12.5 4.5L16 8L12.5 11.5Z" fill="#ffffff"></path> <path d="M2.82843 8L4.91421 10.0858L3.5 11.5L0 8L3.5 4.5L4.91421 5.91422L2.82843 8Z" fill="#ffffff"></path> </g></svg>
+                    </span>
+                    <h3 class="mt-3">Programming</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, laborum!</p>
+                </div>
+
+                <!-- Kolom kedua -->
+                <div class="col-md-4">
+                    <span class="circle">
+                        <svg fill="#ffffff" height="90px" width="90px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 325.04 325.04" xml:space="preserve" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M117.866,234.088c-2.956,14.532-4.875,21.558-16.092,22.458c-2.764,0.222-5.015,2.308-5.446,5.047 c-0.432,2.738,1.069,5.416,3.631,6.477c0.721,0.298,17.877,7.308,37.921,7.309c0.003,0,0.005,0,0.007,0 c13.968,0,25.95-3.386,35.612-10.063c11.45-7.912,19.344-20.294,23.541-36.788l-38.572-38.88 C125.871,194.924,121.253,217.436,117.866,234.088z"></path> <path d="M322.745,63.336c-1.037-1.046-2.887-2.293-5.806-2.293c-3.423,0-12.516,0-67.74,46.992 c-25.509,21.706-54.92,48.559-78.314,71.41l36.603,36.894c24.061-25.009,52.129-56.355,74.451-83.258 c14.096-16.986,24.935-31.002,32.216-41.657C323.799,77.311,328.023,68.655,322.745,63.336z"></path> <path d="M182.595,278.479c-12.387,8.56-27.429,12.899-44.716,12.899c-22.753-0.001-41.919-7.649-44.046-8.527 c-9.425-3.906-14.898-13.673-13.31-23.749c1.555-9.871,9.463-17.373,19.341-18.446c0.861-2.571,1.813-7.254,2.323-9.758 c1.878-9.23,4.449-21.873,12.358-33.126c8.637-12.287,21.656-20.165,38.751-23.466c9.811-9.737,21.005-20.443,32.686-31.308 c-5.905-1.281-11.185-5.127-14.017-10.944c-4.875-10.02-0.623-22.073,9.484-26.895c10.133-4.834,22.287-0.612,27.155,9.423 c0.961,1.978,1.555,4.033,1.832,6.096c9.688-8.677,19.309-17.099,28.392-24.828c0.054-0.046,0.105-0.09,0.16-0.136 c-10.209-19.536-24.849-36.845-42.687-50.098c-25.614-19.031-56.114-29.096-88.2-29.104c-0.01,0-0.017,0-0.025,0 c-21.654,0-47.976,7.566-68.697,19.749C13.981,51.193-0.005,71.163,0,92.49c0.008,25.748,14.53,36.518,26.199,45.171 c9.515,7.057,17.03,12.63,17.034,24.844c0.003,12.213-7.508,17.781-17.018,24.831c-11.665,8.648-26.184,19.412-26.176,45.163 c0.006,21.324,14.001,41.299,39.406,56.244c20.736,12.198,47.072,19.78,68.73,19.786c0.015,0,0.028,0,0.042,0 c39.305,0,76.254-15.171,104.044-42.72c20.837-20.655,34.656-46.416,40.273-74.442c-13.952,15.471-27.997,30.493-40.563,43.322 C206.641,253.965,196.773,268.682,182.595,278.479z M111.054,77.103c2.498-10.871,13.4-17.657,24.354-15.167 c10.939,2.478,17.793,13.282,15.313,24.138c-2.499,10.844-13.407,17.631-24.362,15.154 C115.411,98.764,108.554,87.947,111.054,77.103z M45.054,114.152c-7.005-8.716-5.565-21.401,3.216-28.339 c8.78-6.925,21.571-5.505,28.589,3.195c6.99,8.703,5.545,21.388-3.229,28.34C64.869,124.288,52.058,122.853,45.054,114.152z M55.746,247.168c-8.786-6.944-10.231-19.629-3.226-28.342c7-8.696,19.796-10.122,28.581-3.18 c8.778,6.943,10.224,19.629,3.225,28.327C77.327,252.686,64.53,254.111,55.746,247.168z"></path> </g> </g></svg>
+                    </span>
+                    <h3 class="mt-3">Design</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, laborum!</p>
+                </div>
+
+                <!-- Kolom ketiga -->
+                <div class="col-md-4">
+                    <span class="circle">
+                        <svg fill="#ffffff" height="100px" width="100px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M298.667,302.933c-5.12,0-8.533,3.413-8.533,8.533v25.6c0,5.12,3.413,8.533,8.533,8.533s8.533-3.413,8.533-8.533v-25.6 C307.2,306.347,303.787,302.933,298.667,302.933z"></path> </g> </g> <g> <g> <path d="M332.8,268.8c-5.12,0-8.533,3.413-8.533,8.533v59.733c0,5.12,3.413,8.533,8.533,8.533c5.12,0,8.533-3.413,8.533-8.533 v-59.733C341.333,272.213,337.92,268.8,332.8,268.8z"></path> </g> </g> <g> <g> <path d="M366.933,285.867c-5.12,0-8.533,3.413-8.533,8.533v42.667c0,5.12,3.413,8.533,8.533,8.533s8.533-3.413,8.533-8.533V294.4 C375.467,289.28,372.053,285.867,366.933,285.867z"></path> </g> </g> <g> <g> <path d="M401.067,226.133c-5.12,0-8.533,3.413-8.533,8.533v102.4c0,5.12,3.413,8.533,8.533,8.533c5.12,0,8.533-3.413,8.533-8.533 v-102.4C409.6,229.547,406.187,226.133,401.067,226.133z"></path> </g> </g> <g> <g> <path d="M435.2,260.267c-5.12,0-8.533,3.413-8.533,8.533v68.267c0,5.12,3.413,8.533,8.533,8.533s8.533-3.413,8.533-8.533V268.8 C443.733,263.68,440.32,260.267,435.2,260.267z"></path> </g> </g> <g> <g> <path d="M166.4,123.733c-53.76,0-98.133,44.373-98.133,98.133S112.64,320,166.4,320s98.133-44.373,98.133-98.133 S220.16,123.733,166.4,123.733z M166.4,302.933c-44.373,0-81.067-36.693-81.067-81.067c0-44.373,36.693-81.067,81.067-81.067 s81.067,36.693,81.067,81.067S210.773,302.933,166.4,302.933z"></path> </g> </g> <g> <g> <path d="M236.373,152.747c-19.627-19.627-46.933-29.867-74.24-29.013c-2.56,0-4.267,0.853-5.973,2.56 c-1.707,1.707-2.56,3.413-2.56,5.973v93.867c0,4.267,3.413,8.533,8.533,8.533H256c1.707,0,4.267-0.853,6.827-1.707 c1.707-1.707,2.56-3.413,2.56-5.973C266.24,198.827,256,172.373,236.373,152.747z M170.667,217.6v-76.8 c20.48,0.853,38.4,9.387,52.907,23.893c14.507,14.507,23.04,32.427,23.893,52.907H170.667z"></path> </g> </g> <g> <g> <path d="M231.253,283.307l-63.147-63.147c-3.413-3.413-8.533-3.413-11.947,0s-3.413,8.533,0,11.947l63.147,63.147 c1.707,1.707,3.413,2.56,5.973,2.56s4.267-0.853,5.973-2.56C234.667,291.84,234.667,286.72,231.253,283.307z"></path> </g> </g> <g> <g> <path d="M392.533,157.867h-68.267c-5.12,0-8.533,3.413-8.533,8.533c0,5.12,3.413,8.533,8.533,8.533h68.267 c5.12,0,8.533-3.413,8.533-8.533C401.067,161.28,397.653,157.867,392.533,157.867z"></path> </g> </g> <g> <g> <path d="M442.027,160.427c-0.853,0-0.853-0.853-1.707-0.853c-0.853,0-0.853-0.853-1.707-0.853 c-0.853-0.853-0.853-0.853-1.707-0.853c-2.56,0-5.973,0.853-7.68,2.56s-2.56,3.413-2.56,5.973c0,1.707,0.853,2.56,0.853,3.413 s0.853,1.707,1.707,2.56c1.707,1.707,3.413,2.56,5.973,2.56c0.853,0,0.853,0,0.853,0c0.853-0.853,0.853-0.853,1.707-0.853 s1.707-0.853,1.707-0.853c0.853,0,1.707-0.853,1.707-0.853c1.707-1.707,2.56-3.413,2.56-5.973S442.88,162.133,442.027,160.427z"></path> </g> </g> <g> <g> <path d="M435.2,115.2h-25.6c-5.12,0-8.533,3.413-8.533,8.533s3.413,8.533,8.533,8.533h25.6c5.12,0,8.533-3.413,8.533-8.533 S440.32,115.2,435.2,115.2z"></path> </g> </g> <g> <g> <path d="M341.333,115.2h-17.067c-5.12,0-8.533,3.413-8.533,8.533s3.413,8.533,8.533,8.533h17.067c5.12,0,8.533-3.413,8.533-8.533 S346.453,115.2,341.333,115.2z"></path> </g> </g> <g> <g> <path d="M381.44,117.76c-0.853-0.853-1.707-0.853-2.56-1.707c-3.413-1.707-6.827-0.853-9.387,1.707 c-1.707,1.707-2.56,3.413-2.56,5.973s0.853,4.267,2.56,5.973s3.413,2.56,5.973,2.56s4.267-0.853,5.973-2.56 c1.707-1.707,2.56-3.413,2.56-5.973S383.147,119.467,381.44,117.76z"></path> </g> </g> <g> <g> <path d="M503.467,371.2h-179.2c-5.12,0-8.533,3.413-8.533,8.533v8.533H196.267v-8.533c0-5.12-3.413-8.533-8.533-8.533H8.533 c-5.12,0-8.533,3.413-8.533,8.533v25.6c0,14.507,11.093,25.6,25.6,25.6h460.8c14.507,0,25.6-11.093,25.6-25.6v-25.6 C512,374.613,508.587,371.2,503.467,371.2z M494.933,405.333c0,5.12-3.413,8.533-8.533,8.533H25.6 c-5.12,0-8.533-3.413-8.533-8.533v-17.067H179.2v8.533c0,5.12,3.413,8.533,8.533,8.533h136.533c5.12,0,8.533-3.413,8.533-8.533 v-8.533h162.133V405.333z"></path> </g> </g> <g> <g> <path d="M452.267,81.067H59.733c-14.507,0-25.6,11.093-25.6,25.6v273.067c0,5.12,3.413,8.533,8.533,8.533H179.2v8.533 c0,5.12,3.413,8.533,8.533,8.533h136.533c5.12,0,8.533-3.413,8.533-8.533v-8.533h136.533c5.12,0,8.533-3.413,8.533-8.533V106.667 C477.867,92.16,466.773,81.067,452.267,81.067z M460.8,371.2H324.267c-5.12,0-8.533,3.413-8.533,8.533v8.533H196.267v-8.533 c0-5.12-3.413-8.533-8.533-8.533H51.2V106.667c0-5.12,3.413-8.533,8.533-8.533h392.533c5.12,0,8.533,3.413,8.533,8.533V371.2z"></path> </g> </g> </g></svg>
+                    </span>
+                    <h3 class="mt-3">Consulting</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, laborum!</p>
+                </div>
+            </div> <!-- End of row -->
+        </div>
+    </div>
+
+    
+
+    
+    <div class="container-fluid staff pt-5 pb-5">
+        <div class="container text-center">
+            <h2 class="display-3" id="staff">Our Team</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nesciunt.</p>
+
+            <div class="row">
+                <!-- Kolom pertama -->
+                <div class="col-md-4">
+                    <span class="circle2">
+                        <img src="image/teaam1.png" alt="">
+                    </span>
+                    <h3 class="mt-3">Ryan</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, laborum!</p>
+                </div>
+
+                <!-- Kolom kedua -->
+                <div class="col-md-4">
+                    <span class="circle2">
+                        <img src="image/team2.png" alt="">
+                    </span>
+                    <h3 class="mt-3">Alex</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, laborum!</p>
+                </div>
+
+                <!-- Kolom ketiga -->
+                <div class="col-md-4">
+                    <span class="circle2">
+                        <img src="image/team2.png"  alt="">
+                    </span>
+                    <h3 class="mt-3">Dave</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, laborum!</p>
+                </div>
+            </div> <!-- End of row -->
+
+        </div>
+    </div>
+    
+    
+
+      <footer class="custom-footer">
+        <div class="container">
+          <div class="row">
+            <!-- Section 1 -->
+            <div class="col-md-4 mb-4">
+              <h5>About Us</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Curabitur ac nisi id lacus blandit scelerisque. Fusce volutpat 
+                dignissim felis, nec iaculis libero dapibus sit amet.
+              </p>
+              <p>
+                Quisque vitae ligula id turpis pellentesque faucibus. 
+                Nullam sit amet velit vel sapien tincidunt tristique.
+              </p>
+            </div>
+    
+            <!-- Section 2 -->
+            <div class="col-md-4 mb-4">
+              <h5>Quick Links</h5>
+              <ul class="list-unstyled">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+              </ul>
+            </div>
+    
+            <!-- Section 3 -->
+            <div class="col-md-4 mb-4">
+              <h5>Contact Us</h5>
+              <p>
+                Address: 1234 Street Name, City, Country <br>
+                Phone: <a href="tel:+123456789">+123 456 789</a> <br>
+                Email: <a href="mailto:info@yourcompany.com">info@yourcompany.com</a>
+              </p>
+              <p>Follow us on:</p>
+              <a href="https://facebook.com" target="_blank" class="me-2">Facebook</a>
+              <a href="https://twitter.com" target="_blank" class="me-2">Twitter</a>
+              <a href="https://instagram.com" target="_blank">Instagram</a>
+            </div>
+          </div>
+    
+          <hr class="bg-white">
+          <div class="text-center">
+            <p class="mb-0">&copy; 2025 Your Company Name. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
+
+      <a href="https://wa.me/123456789" target="_blank" class="whatsapp-float">
+    <img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt="WhatsApp" />
+</a>
+ 
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
