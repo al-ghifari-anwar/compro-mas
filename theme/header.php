6c9ff06e495d52598f68b831f6f6d1b0ff30f24f
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-02-10 07:40:40 +0700

    SEO

--- theme/header.php
+++ theme/header.php
@@ -4,10 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>nova</title>
+    <title>Miraswift</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <meta name="keywords" content="Miraswift, PT Miraswift Auto Solusi, PT. Miraswift Auto Solusi">
+    <meta name="description" content="Official website miraswift">
 </head>
 
 <body>
